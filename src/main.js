4578e41ebc09eda8cf5f8efc073bae1089348173
14796028027 <1491870986@qq.com>
2023-05-29 22:18:35 +0800

    静态资源创建

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+//引入初始化样式
+import '@/styles/common.scss'
+
+
+
 import { getCategory } from './apis/testAPI'
 
 getCategory().then(res =>{
