6d8086402b0b953c8dc91a60572c94eaf46537c7
14796028027 <1491870986@qq.com>
2023-05-29 10:37:56 +0800

    添加sacc

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 //element-plus按需引入
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { ElementPlusResolver, ElementUiResolver } from 'unplugin-vue-components/resolvers'
 
 
 // https://vitejs.dev/config/
@@ -17,12 +17,23 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        //sacc：1.配置elementPlus采用sass样式配色系统
+        ElementPlusResolver({importStyle:'sass'}),
+      ],
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css:{
+    preprocessorOptions:{
+      scss:{
+        //sacc：2.自动导入自定义样式文件进行覆盖
+        additionalData:`@use "@/styles/element/index.scss" as *;`,
+      }
     }
   }
 })
