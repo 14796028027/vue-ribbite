2ed98ed346f7dfb38fa9b94b375ac909a8ba2968
14796028027 <1491870986@qq.com>
2023-05-31 17:23:14 +0800

    GoodsItem简单封装

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -2,7 +2,7 @@
 import HomePanel from './HomePanel.vue'
 import {getGoodsAPI} from '@/apis/home'
 import {ref} from 'vue'
-
+import GoodesItem from "@/views/Home/components/Goodsitem.vue"
 
 const getGoodsList = ref([])
 const getGoods = async () => {
@@ -27,12 +27,7 @@ getGoods()
         </RouterLink>
         <ul class="goods-list">
           <li v-for="good in cate.goods" :key="good.id">
-            <RouterLink to="/" class="goods-item">
-              <img v-img-lazy="good.picture" alt="" />
-              <p class="name ellipsis">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-            </RouterLink>
+            <GoodesItem :goods="good"/>
           </li>
         </ul>
       </div>
