d2dc64bd2d8be8331d86c10b41e94adb40588e9d
14796028027 <1491870986@qq.com>
2023-05-31 16:17:44 +0800

    product列表渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -5,7 +5,6 @@ import HomeBanner from '@/views/Home/components/HomeBanner.vue'
 import HomeNew from '@/views/Home/components/HomeNew.vue'
 import HomeHot from '@/views/Home/components/HomeHot.vue'
 import homeProduct from '@/views/Home/components/HomeProduct.vue'
-import HomePanel from '@/views/Home/components/HomePanel.vue'
 </script>
 
 <template>
@@ -16,14 +15,4 @@ import HomePanel from '@/views/Home/components/HomePanel.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct /> 
-  <HomePanel title="新鲜好物" sub-tiele="新鲜好物 好多商品">
-    <div>
-      新鲜好物的插槽
-    </div>
-  </HomePanel>
-  <HomePanel title="人气推荐" sub-tiele="人气推荐 好多商品">
-    <div>
-      人去推荐的插槽
-    </div>
-  </HomePanel>
 </template>
